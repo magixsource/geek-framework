98abbba7039dcbefb7faed795c210ffa3e7de72e
magixsource <magixsource.cn@gmail.com>
2023-12-26 16:25:51 +0800

    annotation增加声明

--- src/main/java/gl/linpeng/gf/annotation/Auth.java
+++ src/main/java/gl/linpeng/gf/annotation/Auth.java
@@ -1,7 +1,12 @@
 package gl.linpeng.gf.annotation;
 
+import java.lang.annotation.*;
+
 /**
  * Mark this method should be a auth method
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
 public @interface Auth {
 }
